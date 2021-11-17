e3e57480abeee03e1a4c04cd825adcd368f74e33
Nathan <nathanrajkumar@gmail.com>
2021-11-17 14:58:21 -0500

    added poker hands file

---
+++ src/main/SetExerciseApplication.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.Set;
+
+
+public class SetExerciseApplication {
+	
+	// instatiate new hashset
+
+	public static void main(String[] args) {
+		
+		// extract the data from the CSV
+		
+		
+		// remove ACE HIGH and QUEEN HIGH
+		
+		
+		// Update PAIR to DEUCES
+		
+	}
+	
+	public static void printToConsole() {
+
+	}
+	
+	public static Set<String> extractDataFromCSV(String fileName) {
+
+		return null;
+	}
+
+}
